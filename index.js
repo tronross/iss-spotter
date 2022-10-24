b55581bb6fc03c6c705bf731fc32eac46374e865
tronross <john_ross@analogforever.ca>
2022-10-23 23:43:10 -0400

    Add logic to extract flyOverTimes and convert them to human readable format for the user

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 // index.js
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+// const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
 
+const { nextISSTimesForMyLocation } = require('./iss');
+
+
+const printFlyOvers = function(flyOvers) {
+  for (const fly of flyOvers) {
+    const whenFly = new Date(0);
+    whenFly.setUTCSeconds(fly.risetime);
+    const flyDuration = fly.duration;
+    console.log(`The ISS will next fly overhead at ${whenFly} and will be visible for ${flyDuration} seconds`);
+  }
+};
+
+
+nextISSTimesForMyLocation((error, flyOvers) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  printFlyOvers(flyOvers);
+});
 
 
 
